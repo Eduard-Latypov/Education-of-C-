03420f7df0480301adb41f65e613ce35c89790b4
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-11-29 14:11:08 +0300

    Information and program of Convert!

---
+++ test_2/Program.cs
@@ -0,0 +1,9 @@
+﻿string x = Console.ReadLine();
+string y = Console.ReadLine();
+double result = Convert.ToDouble(x) + Convert.ToDouble(y);
+double div = Convert.ToDouble(x) / Convert.ToDouble(y);
+Console.WriteLine(result);
+Console.WriteLine(div);
+
+
+
