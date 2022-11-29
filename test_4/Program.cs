70f00de682f06502f9ce8fa8f6f056354f111e0e
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-11-29 14:12:51 +0300

    Переменные, назначение и переназначение

---
+++ test_4/Program.cs
@@ -0,0 +1,18 @@
+﻿bool variable = false;
+System.Console.WriteLine(variable);
+variable = true;
+System.Console.WriteLine(variable);
+
+int a, b, c, d;
+
+a = 100;
+System.Console.WriteLine(a);
+a = 250;
+System.Console.WriteLine(a);
+b = 29;
+c = 38;
+d = 384;
+System.Console.WriteLine(b);
+System.Console.WriteLine(c);
+System.Console.WriteLine(d);
+
