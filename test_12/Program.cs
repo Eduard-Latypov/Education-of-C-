00e76c9c2e30831fdaf9cf1e24f9947be7bfe2e8
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-11-30 16:09:17 +0300

    оператор break and continue

---
+++ test_12/Program.cs
@@ -0,0 +1,12 @@
+﻿while (true)
+{
+    System.Console.WriteLine("Введите что-нибудь: ");
+    string word = Console.ReadLine();
+    System.Console.Write("Вы ввели: ");
+    System.Console.WriteLine(word);
+    if (word == "exit")
+    {
+        break;
+    }
+}
+System.Console.WriteLine("Всего доброго!!!");
