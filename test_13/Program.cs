6c8f4f308216f20418db1170b3f6ad4471ec2093
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-11-30 21:40:20 +0300

    вложенные циклы for

---
+++ test_13/Program.cs
@@ -0,0 +1,18 @@
+﻿for (int i = 1; i <= 10; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        System.Console.Write("#");
+    }
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
+System.Console.WriteLine();
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 10; j >= i; j--)
+    {
+        System.Console.Write("#");
+    }
+    System.Console.WriteLine();
+}
