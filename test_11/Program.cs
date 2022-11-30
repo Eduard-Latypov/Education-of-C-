4fa2f30b583f6984e59c456687cee55d71bf4504
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-11-30 15:36:28 +0300

    вариации цикла for

---
+++ test_11/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+for (int i = 0; i <= 10; i++)
+{
+    System.Console.Write(i + " ");
+}
+System.Console.WriteLine();
+for (int i = 0, j = 0; i < 10; i++, j++)
+{
+    System.Console.Write(i + " ");
+    System.Console.Write(j + " ");
+}
+System.Console.WriteLine();
+for (int i = 0, j = 5; i < 10 && j < 12; i++, j++)
+{
+    System.Console.Write(i + " ");
+    System.Console.Write(j + " ");
+}
+System.Console.WriteLine();
+for (int i = 0, j = 5; i < 10 || j < 12; i++, j++)
+{
+    System.Console.Write(i + " ");
+    System.Console.Write(j + " ");
+}
+System.Console.WriteLine();
+for (int i = 10, j = 0; i >= 0; i--, j++)
+{
+    System.Console.Write(i + " ");
+    System.Console.Write(j + " ");
+}
+// for (;;)
+// {
+
+// }     бесконечный цикл!!!   
+
