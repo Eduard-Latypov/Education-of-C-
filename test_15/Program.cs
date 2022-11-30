76aec3766e9646a4fdbe15d9b3df4fcff9a79eef
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-11-30 22:35:04 +0300

    Массивы

---
+++ test_15/Program.cs
@@ -0,0 +1,10 @@
+﻿// массивы
+
+int[] myList;                // объявляем массив
+myList = new int[10];       //  выделяем в оперативной памяти в управляемой куче память для массива 
+myList[0] = 1;              // из 10 элементов
+myList[1] = 2;
+myList[2] = 3;
+System.Console.WriteLine(myList[2]);
+System.Console.WriteLine(myList[9]);
+
