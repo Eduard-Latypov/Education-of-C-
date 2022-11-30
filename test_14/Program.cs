111c7beee5ddd5eebaa706c71309d37b1af2a397
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-11-30 21:51:56 +0300

    тернарный оператор

---
+++ test_14/Program.cs
@@ -0,0 +1,12 @@
+﻿// тернарный оператор 
+// конструкция  [тип переменная = (условие) ? значение если условие true : значение если условие false]
+
+System.Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+string word = num % 2 == 0 ? "this number is even" : "this number is odd";
+System.Console.WriteLine(word);
+
+
+
+
+
