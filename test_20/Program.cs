8e9d6b418185a61c2418742361d76ec77eda3641
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-12-03 14:46:59 +0300

    Зубчатые массивы

---
+++ test_20/Program.cs
@@ -0,0 +1,16 @@
+﻿int[][] myArray = new int[5][];
+myArray[0] = new int[4];
+myArray[1] = new int[5];
+myArray[2] = new int[6];
+myArray[3] = new int[7];
+myArray[4] = new int[8];
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    for (int j = 0; j < myArray[i].Length; j++)
+    {
+        myArray[i][j] = new Random().Next(1, 100);
+        System.Console.Write(myArray[i][j] + "\t");
+    }
+    System.Console.WriteLine();
+}
