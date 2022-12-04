509f860135c799f7aa030e76fded2a4ae95c3ade
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-12-04 11:41:23 +0300

    трехмерные массивы(создание, заполнение, вывод)

---
+++ test_21/Program.cs
@@ -0,0 +1,34 @@
+﻿int[,,] createArray()
+{
+    int[,,] myArray = new int[new Random().Next(1, 5), new Random().Next(1, 5), new Random().Next(1, 5)];
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < myArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < myArray.GetLength(2); k++)
+            {
+                myArray[i, j, k] = new Random().Next(100);
+            }
+        }
+    }
+    return myArray;
+}
+
+void printArray(int[,,] myArray)
+{
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < myArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < myArray.GetLength(2); k++)
+            {
+                System.Console.Write(myArray[i, j, k] + "\t");
+            }
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+printArray(createArray());
