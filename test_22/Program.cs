424f092d351fabbb50e84c0c614bf19882c78ebf
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-12-04 11:53:12 +0300

    Трехмерный зубчатый массив (создание, заполнение, вывод)

---
+++ test_22/Program.cs
@@ -0,0 +1,36 @@
+﻿int[][][] createArray()
+{
+    int[][][] myArray = new int[new Random().Next(2, 6)][][];
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        myArray[i] = new int[new Random().Next(2, 6)][];
+        for (int j = 0; j < myArray[i].Length; j++)
+        {
+            myArray[i][j] = new int[new Random().Next(2, 6)];
+            for (int k = 0; k < myArray[i][j].Length; k++)
+            {
+                myArray[i][j][k] = new Random().Next(100);
+            }
+        }
+    }
+    return myArray;
+}
+
+void printArray(int[][][] myArray)
+{
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        for (int j = 0; j < myArray[i].Length; j++)
+        {
+            for (int k = 0; k < myArray[i][j].Length; k++)
+            {
+                System.Console.Write(myArray[i][j][k] + " ");
+            }
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+printArray(createArray());
