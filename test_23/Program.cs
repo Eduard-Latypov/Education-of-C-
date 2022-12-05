84e616e06cfc8599919dd76d855f8e9940594b2a
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-12-05 14:40:43 +0300

    Null-объединение

---
+++ test_23/Program.cs
@@ -0,0 +1,7 @@
+﻿string name = "Eduard";
+System.Console.WriteLine(name);
+string secondName = "Latypov";
+System.Console.WriteLine(secondName);
+string middleName = null;
+System.Console.WriteLine(middleName ?? "нет отчества"); // устанавливаем дефолтное значение для переменной 
+                                                        // под названием middleName, то есть если в переменной будет какое либо значение, то оно выведется на консоль, если же там будет null то выведется нет отчества!!!
