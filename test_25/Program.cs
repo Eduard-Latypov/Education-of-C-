ea9ba0a9cce55898c35a4bffee2e0b2db0d1f03b
Eduard Latypov <LatypovEdik1988@yandex.ru>
2022-12-06 15:36:18 +0300

    переполнение

---
+++ test_25/Program.cs
@@ -0,0 +1,14 @@
+﻿int num = int.MaxValue;
+//int result = num + 1;   // получим переполнение и откатимся к минимальному значению
+// int result = checked(num + 1);   // получим ошибку переполнения
+//System.Console.WriteLine(result);
+
+try
+{
+    int count = checked(num + 1);
+    System.Console.WriteLine(count);
+}
+catch (System.Exception)
+{
+    System.Console.WriteLine("Ошибка переполнения!");
+}
